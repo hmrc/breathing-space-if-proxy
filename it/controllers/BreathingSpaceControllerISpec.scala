45b46dbc2f00cbe280a7f445c5e47d6f606912a0
glidester <timothy.wilkins@digital.hmrc.gov.uk>
2020-08-24 09:15:53 +0100

    [BS-131][TW] Adding IT tests

---
+++ it/controllers/BreathingSpaceControllerISpec.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import play.api.http.Status
+import play.api.test.Helpers._
+import play.api.test.WsTestClient
+
+class BreathingSpaceControllerISpec extends BaseControllerISpec {
+
+  private val anino = "ABCDE"
+
+  WsTestClient.withClient { client =>
+    "GET /debtor/:nino/debt-details" in {
+      val result = await(
+        client.url(s"http://localhost:$port/breathing-space/debtor/${anino}/debt-details")
+          .addHttpHeaders("Content-Type" -> "application/json")
+          .get()
+      )
+
+      result.status shouldBe Status.OK
+
+    }
+  }
+
+  /*
+  "POST /breathing-space-period" should {
+    "return 200" in {
+      val result = controller.createBreathingSpacePeriod()(fakeRequest)
+      status(result) shouldBe Status.OK
+    }
+  }*/
+}
