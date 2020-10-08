bbf6209c6627bee3f31aeb825210647ce3efe4a2
Lucio Biondi <lucio.biondi@digital.hmrc.gov.uk>
2020-10-08 13:41:50 +0100

    [BS-348][LB] Remove date constraints for POST and PUT Periods in BS_Proxy

--- app/uk/gov/hmrc/breathingspaceifproxy/model/ErrorItem.scala
+++ app/uk/gov/hmrc/breathingspaceifproxy/model/ErrorItem.scala
@@ -25,13 +25,10 @@ sealed abstract class BaseError(val httpCode: Int, val message: String) extends 
 object BaseError extends Enum[BaseError] {
 
   case object INVALID_BODY extends BaseError(BAD_REQUEST, "Not expected a body to this endpoint")
-  case object INVALID_DATE extends BaseError(BAD_REQUEST, "Invalid date value for period ")
-  case object INVALID_DATE_RANGE extends BaseError(BAD_REQUEST, "End-date before start-date for period ")
   case object INVALID_HEADER extends BaseError(BAD_REQUEST, "Invalid value for the header")
   case object INVALID_JSON extends BaseError(BAD_REQUEST, "Payload not in the expected Json format")
   case object INVALID_JSON_ITEM extends BaseError(BAD_REQUEST, "One or more values cannot validated for the Json item")
   case object INVALID_NINO extends BaseError(BAD_REQUEST, "Invalid Nino")
-  case object INVALID_TIMESTAMP extends BaseError(BAD_REQUEST, "Timestamp is too old for period ")
   case object MISSING_BODY extends BaseError(BAD_REQUEST, "The request must have a body")
   case object MISSING_HEADER extends BaseError(BAD_REQUEST, "Missing required header")
   case object MISSING_NINO extends BaseError(BAD_REQUEST, "Payload does not contain a 'nino' value?")
