de38bef741323f3741b30fd253f2210d40b9faa4
Lucio Biondi <lucio.biondi@digital.hmrc.gov.uk>
2020-11-24 16:15:02 +0000

    [BS-436][LB] Update the library dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,10 +1,23 @@
-import play.core.PlayVersion.current
+import play.core.PlayVersion
 import sbt._
 
 object Dependencies {
 
+  object version {
+    val jackson = "2.11.3"
+  }
+
   val compile = Seq(
-    "uk.gov.hmrc"   %% "bootstrap-backend-play-27" % "2.25.0",
+    "com.fasterxml.jackson.core"     %  "jackson-annotations"      % version.jackson,
+    "com.fasterxml.jackson.core"     %  "jackson-core"             % version.jackson,
+    "com.fasterxml.jackson.core"     %  "jackson-databind"         % version.jackson,
+    "com.fasterxml.jackson.datatype" %  "jackson-datatype-jdk8"    % version.jackson,
+    "com.fasterxml.jackson.datatype" %  "jackson-datatype-jsr310"  % version.jackson,
+    "com.fasterxml.jackson.module"   %  "jackson-module-parameter-names" % version.jackson,
+    "com.fasterxml.jackson.module"   %  "jackson-module-paranamer" % version.jackson,
+    "com.fasterxml.jackson.module"   %% "jackson-module-scala"     % version.jackson,
+
+    "uk.gov.hmrc"   %% "bootstrap-backend-play-27" % "3.0.0",
     "org.typelevel" %% "cats-core"                 % "2.2.0",
     "com.beachape"  %% "enumeratum"                % "1.6.1",
     "com.kenshoo"   %% "metrics-play"              % "2.7.3_0.8.2",
@@ -12,12 +25,12 @@ object Dependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-27"   % "2.25.0"  % Test,
-    "com.typesafe.play"      %% "play-test"                % current   % Test,
-    "org.scalatest"          %% "scalatest"                % "3.2.2"   % Test,
-    "com.vladsch.flexmark"   %  "flexmark-all"             % "0.35.10" % "test, it",
+    "uk.gov.hmrc"            %% "bootstrap-test-play-27"   % "3.0.0"   % Test,
+    "com.typesafe.play"      %% "play-test"                % PlayVersion.current % Test,
+    "org.scalatest"          %% "scalatest"                % "3.2.3"   % Test,
+    "com.vladsch.flexmark"   %  "flexmark-all"             % "0.36.8"  % "test, it",
     "org.scalatestplus.play" %% "scalatestplus-play"       % "4.0.3"   % "test, it",
-    "com.github.tomakehurst" %  "wiremock-jre8"            % "2.26.3"  % "test, it",
-    "org.mockito"            %% "mockito-scala-scalatest"  % "1.15.0"  % "test, it"
+    "com.github.tomakehurst" %  "wiremock-jre8"            % "2.27.2"  % "test, it",
+    "org.mockito"            %% "mockito-scala-scalatest"  % "1.16.3"  % "test, it"
   )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -11,6 +11,7 @@ addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.0.0")
 addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "2.1.0")
 
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.7.5")
+addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.5.1")
 addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.3.0")
 addSbtPlugin("org.scalastyle" % "scalastyle-sbt-plugin" % "1.0.0")
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.6.1")
